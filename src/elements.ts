96422ef54fba2c5b4bcb2b350e42b2b690d91682
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2023-10-22 22:44:59 -0700

    step 7

--- src/elements.ts
+++ src/elements.ts
@@ -1,35 +1,64 @@
 interface Coordinate {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
 export class Line {
-  coords: Coordinate[];
-  thickness: string;
+    coords: Coordinate[];
+    thickness: string;
 
-  constructor(thickness = "1") {
-    this.coords = [];
-    this.thickness = thickness;
-  }
-
-  drag(x: number, y: number) {
-    this.coords.push({ x: x, y: y });
-  }
-
-  display(ctx: CanvasRenderingContext2D) {
-    if (this.coords.length == 0) {
-      return;
-    }
-    ctx.lineWidth = parseInt(this.thickness);
-    const first = this.coords[0];
-
-    ctx.beginPath();
-    ctx.moveTo(first.x, first.y);
-
-    for (const c of this.coords) {
-      ctx.lineTo(c.x, c.y);
+    constructor(thickness = "1") {
+        this.coords = [];
+        this.thickness = thickness;
     }
 
-    ctx.stroke();
-  }
+    drag(x: number, y: number) {
+        this.coords.push({ x: x, y: y });
+    }
+
+    display(ctx: CanvasRenderingContext2D) {
+        if (this.coords.length == 0) {
+            return;
+        }
+        ctx.lineWidth = parseInt(this.thickness);
+        const first = this.coords[0];
+
+        ctx.beginPath();
+        ctx.moveTo(first.x, first.y);
+
+        for (const c of this.coords) {
+            ctx.lineTo(c.x, c.y);
+        }
+
+        ctx.stroke();
+    }
+}
+
+
+export class CursorCommand {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    display(ctx: CanvasRenderingContext2D) {
+        //16-64 from 1-11
+        const outMin = 16;
+        const outMax = 64;
+        const inMin = 1;
+        const inMax = 11;
+        const newSize: number =
+            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+            ((ctx.lineWidth - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+        const xOffset = (4 * newSize) / outMin;
+        const yOffset = (8 * newSize) / outMin;
+
+        ctx.font = newSize + "px monospace";
+        ctx.fillStyle = "black";
+
+        ctx.fillText("*", this.x - xOffset, this.y + yOffset);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const drawingChanged = new CustomEvent("drawing-changed");
-import { Line } from "./elements";
+import { Line, CursorCommand } from "./elements";
 //GAME SETUP/////////////////////////////////////
 const gameName = "Doodle It";
 document.title = gameName;
@@ -12,10 +12,11 @@ header.innerHTML = gameName;
 app.append(header);
 type ClickHandler = () => void;
 const cursor = { active: false, x: 0, y: 0 };
+const cursorChanged = new CustomEvent("cursor-changed");
 let drawing: Line[] = [];
 let currentDrawing: Line | null = new Line();
 let redoDrawing: Line[] = [];
-
+let cursorCommand: CursorCommand | null = null;
 //////////////////////////////////////////////
 
 ///////////BUTTONS//////////////////
@@ -36,6 +37,7 @@ ctx.fillStyle = "white";
 ctx.fillRect(0, 0, 256, 256);
 addCanvasEvents();
 app.append(document.createElement("br"));
+canvas.style.cursor = "none";
 //////////////////////////////////////////////
 
 app.append(document.createElement("br"));
@@ -43,53 +45,73 @@ const thickSlider = addThicknessSlider();
 
 ///////DRAWING///////////////////
 function addCanvasEvents() {
-  canvas.addEventListener("mousedown", (event) => {
-    cursor.active = true;
-    cursor.x = event.offsetX;
-    cursor.y = event.offsetY;
-    currentDrawing = new Line(thickSlider.value);
-    drawing.push(currentDrawing);
-    redoDrawing.splice(0, redoDrawing.length);
-    currentDrawing.drag(cursor.x, cursor.y);
+    canvas.addEventListener("mousedown", (event) => {
+        cursor.active = true;
+        cursor.x = event.offsetX;
+        cursor.y = event.offsetY;
+        currentDrawing = new Line(thickSlider.value);
+        drawing.push(currentDrawing);
+        redoDrawing.splice(0, redoDrawing.length);
+        currentDrawing.drag(cursor.x, cursor.y);
 
-    canvas.dispatchEvent(drawingChanged);
-  });
+        canvas.dispatchEvent(drawingChanged);
+    });
 
-  canvas.addEventListener("mousemove", (event) => {
-    if (!cursor.active) {
-      return;
-    }
-    cursor.x = event.offsetX;
-    cursor.y = event.offsetY;
-    currentDrawing!.drag(cursor.x, cursor.y);
-    redoDrawing = [];
+    canvas.addEventListener("mousemove", (event) => {
+        if (cursor.active) {
+            cursor.x = event.offsetX;
+            cursor.y = event.offsetY;
+            currentDrawing!.drag(cursor.x, cursor.y);
+            redoDrawing = [];
 
-    canvas.dispatchEvent(drawingChanged);
-  });
+            canvas.dispatchEvent(drawingChanged);
+        }
+    });
 
-  canvas.addEventListener("mouseup", () => {
-    cursor.active = false;
-    currentDrawing = null;
-  });
+    canvas.addEventListener("mouseup", () => {
+        cursor.active = false;
+        currentDrawing = null;
+    });
 
-  canvas.addEventListener("drawing-changed", () => {
-    drawIT();
-  });
+    canvas.addEventListener("drawing-changed", () => {
+        drawIT();
+    });
 }
+
+canvas.addEventListener("cursor-changed", () => {
+    drawIT();
+});
+
+//Cursor events
+canvas.addEventListener("mouseout", () => {
+    cursorCommand = null;
+    canvas.dispatchEvent(cursorChanged);
+});
+
+canvas.addEventListener("mouseenter", (e) => {
+    cursorCommand = new CursorCommand(e.offsetX, e.offsetY);
+    canvas.dispatchEvent(cursorChanged);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    cursorCommand = new CursorCommand(e.offsetX, e.offsetY);
+    canvas.dispatchEvent(cursorChanged);
+});
+
 //////////////////////////////////////////////
 
 //CLEAR BUTTON/////////////////
 function eraseCanvas() {
-  redoDrawing = drawing;
-  drawing = [];
-  clearCanvas();
+    redoDrawing = drawing;
+    drawing = [];
+    clearCanvas();
 }
 
 function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
 
-  ctx.fillRect(0, 0, 256, 256);
+    ctx.fillRect(0, 0, 256, 256);
 }
 
 //////////////////////////////////////////////
@@ -97,68 +119,74 @@ function clearCanvas() {
 /////DRAW IT FOR REAL/////////////////////////
 
 function drawIT() {
-  clearCanvas();
-  for (const l of drawing) {
-    l.display(ctx);
-  }
+    clearCanvas();
+    const lineWidthBefore = ctx.lineWidth;
+    if (cursorCommand) {
+        cursorCommand.display(ctx);
+    }
+    drawing.forEach((l) => {
+        l.display(ctx);
+    });
+    ctx.lineWidth = lineWidthBefore;
 }
 //////////////////////////////////////////////
 
 //////undo///////////////////////////////////
 function undoCanvas() {
-  if (drawing.length == 0) {
-    return;
-  }
-  redoDrawing.push(drawing.pop()!);
-  canvas.dispatchEvent(drawingChanged);
+    if (drawing.length == 0) {
+        return;
+    }
+    redoDrawing.push(drawing.pop()!);
+    canvas.dispatchEvent(drawingChanged);
 }
 ///////////////////////////////////////////////
 
 ///////////////redo///////////////////////////
 function redoCanvas() {
-  if (redoDrawing.length == 0) {
-    return;
-  }
-  drawing.push(redoDrawing.pop()!);
-  canvas.dispatchEvent(drawingChanged);
+    if (redoDrawing.length == 0) {
+        return;
+    }
+    drawing.push(redoDrawing.pop()!);
+    canvas.dispatchEvent(drawingChanged);
 }
 
 ///////////////////////////////////////////////
 
 ///////CLICKING ABLILITY//////////////////////
 function addButton(text: string, funct: ClickHandler) {
-  const button = document.createElement("button");
-  button.innerHTML = text;
-  app.append(button);
+    const button = document.createElement("button");
+    button.innerHTML = text;
+    app.append(button);
 
-  //click
-  button.addEventListener("click", () => {
-    funct();
-  });
+    //click
+    button.addEventListener("click", () => {
+        funct();
+    });
 }
 //////////////////////////////////////////////////
 
 /////////////////THICKNESS SLIDER/////////////////
 function addThicknessSlider() {
-  const thickness = document.createElement("input");
-  thickness.type = "range";
-  thickness.min = "1";
-  thickness.max = "5";
-  thickness.value = "1";
+    const thickness = document.createElement("input");
+    thickness.type = "range";
+    thickness.min = "1";
+    thickness.max = "5";
+    thickness.value = "1";
 
-  thickness.addEventListener("input", () => {
-    changeThickness(parseInt(thickness.value));
-  });
+    thickness.addEventListener("input", () => {
+        changeThickness(parseInt(thickness.value));
+    });
 
-  const label = document.createElement("label");
-  label.textContent = "Thickness: ";
+    const label = document.createElement("label");
+    label.textContent = "Thickness: ";
 
-  app.append(label);
-  app.append(thickness);
-  return thickness;
+    app.append(label);
+    app.append(thickness);
+    return thickness;
 }
 
 function changeThickness(value: number) {
-  ctx.lineWidth = value;
+    ctx.lineWidth = value;
+    drawIT();
 }
 //////////////////////////////////////////////////
